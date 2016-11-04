938ecd74482bca210dd6d306cececf46909f1bf9
Lab1 <lab1@gmail.com>
2016-11-04 08:37:51 +0300

    Initial commit. Project started. Main loop

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	FILE *data;
+	unsigned int sarradas;
+	char input;
+	
+	printf("\n\tWelcome to the Unesp Bright River BCC 2016 Sarrada Counter\n");
+	printf("\tType 's' to increase the number of sarradas\n");
+	printf("\tType 'q' to quit\n");
+	
+		do
+		{
+			
+		}while((input != 'q' ||) || (input != 'Q'));
+	return 0;
+}
